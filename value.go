6a03eaf0aa82ebb98cdb4332f380fb7b35b65701
an-ikhtiyour <ikhtiyarabdukhashimov@Ikhtiyars-MacBook-Air.local>
2022-03-27 10:47:58 +0500

    value and variable added

---
+++ value.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func value() {
+	var a string = "it is string"
+	var b int = 5
+	var c float64 = 4.2
+	var abc bool
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(abc)
+}
